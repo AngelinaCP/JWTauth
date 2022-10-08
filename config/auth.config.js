a0d5f11b0e72bd504807611be8d303f53886289e
AngelinaCP <89658366303@mail.ru>
2022-10-08 15:05:38 +0300

    working auth

---
+++ config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+	secret: "garage-secret-key"
+  };
--- index.js
+++ index.js
@@ -1,22 +1,65 @@
-import express, {json} from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-import {dirname, join} from 'path';
-import {fileURLToPath} from 'url';
-
-dotenv.config();
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
- 
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const db = require("./models");
 const app = express();
-const PORT = process.env.PORT || 3001;
-const corsOptions = {credentials: true, origin: process.env.URL || '*'};
+
+
+const Role = db.role;
+
+const corsOptions = {
+  origin: "http://localhost:3000"
+};
 
 app.use(cors(corsOptions));
-app.use(json());
-app.use(cookieParser());
 
-app.use('/', express.static(join(__dirname, 'public')));
 
-app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+require('./routes/auth.routes')(app);
+require('./routes/user.routes')(app);
+
+
+db.sequelize
+.authenticate()
+.then(() => {
+  console.log('Connection has been established successfully.');
+})
+.catch(err => {
+  console.error('Unable to connect to the database:', err);
+});
+
+
+db.sequelize.sync();
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to jwt test application." });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
+function initial() {
+  Role.create({
+    id: 1,
+    name: "user"
+  });
+ 
+  Role.create({
+    id: 2,
+    name: "moderator"
+  });
+ 
+  Role.create({
+    id: 3,
+    name: "admin"
+  });
+}
+
+// initial();
